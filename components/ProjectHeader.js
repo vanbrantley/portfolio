1db7e94b7b2dfce66b22bdbc5d52c9ea9d3d6ba9
Van Brantley <van.brantley@gmail.com>
2025-12-14 11:37:49 -0500

    update favicon

--- components/ProjectHeader.js
+++ components/ProjectHeader.js
@@ -1,31 +1,3 @@
-// import Image from "next/image";
-
-// export default function ProjectHeader({ title, subtitle, image }) {
-//     return (
-//         <header className="w-full bg-gray-50 rounded-lg mb-8 p-6 text-center">
-//             <div className="w-full max-w-4xl mx-auto aspect-video mb-6 relative bg-white rounded">
-//                 <Image
-//                     src={image}
-//                     alt={title}
-//                     fill
-//                     className="object-contain"
-//                     priority
-//                 />
-//             </div>
-
-//             <h1 className="text-3xl font-bold text-gray-900 mb-2">
-//                 {title}
-//             </h1>
-
-//             {subtitle && (
-//                 <p className="text-gray-700">
-//                     {subtitle}
-//                 </p>
-//             )}
-//         </header>
-//     );
-// }
-
 import Image from "next/image";
 
 export default function ProjectHeader({ title, subtitle, image }) {
