588b5900a8834d6a7b95b47a89bb9cad22ab46b1
Van Brantley <van.brantley@gmail.com>
2026-06-02 18:12:32 -0400

    add logo to navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,15 +1,26 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
 export default function Navbar() {
   return (
-    <nav className="sticky top-0 z-50 bg-white p-4 shadow flex justify-between">
-      <Link href="/">
-        <span className="font-bold cursor-pointer text-gray-900">Van Brantley</span>
+    <nav className="sticky top-0 z-50 bg-white p-4 px-7 shadow flex justify-between items-center">
+
+      <Link href="/" className="flex items-center">
+        <Image
+          src="/images/logo.png"
+          alt="Logo"
+          width={28}
+          height={28}
+          className="cursor-pointer"
+          priority
+        />
       </Link>
-      <div className="space-x-4">
+
+      <div className="space-x-4 text-gray-900">
         <Link href="/">Home</Link>
         <Link href="/about">About</Link>
       </div>
+
     </nav>
   );
 }
